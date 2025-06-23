f4e366a8a98f097ed08c063cbff7c14e01fc2724
Hey <heyhey@Heys-iMac.local>
2025-06-22 21:13:51 -0400

    added documentation

--- api.py
+++ api.py
@@ -4,11 +4,13 @@ from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 @app.get('/')
-def home():
+def home() -> dict:
+    """Defines home endpoint and checks connection with the api."""
     return {"heyStranger": "sup"}
 
 @app.post('/run/')
-def run(img: UploadFile=File(...)):
+def run(img: UploadFile=File(...)) -> dict:
+    """Return model outputs (detection results, connection status, and model messages) for uploaded .jpg picture."""
     with open('uploadedimg.jpg','wb') as pic:
         shutil.copyfileobj(img.file, pic)    
     result = subprocess.run(["python3","run_detector.py", '--input', 'uploadedimg.jpg'], capture_output=True, text=True)
@@ -18,9 +20,10 @@ def run(img: UploadFile=File(...)):
         'status': result.returncode,
         'msg': result.stderr
     }
-#  
+ 
 @app.get("/upload", response_class=HTMLResponse)
-async def upload_form():
+async def upload_form() -> str:
+    """Creates the web interface for API users to upload a .jpg picture."""
     return """
     <html>
         <body>
